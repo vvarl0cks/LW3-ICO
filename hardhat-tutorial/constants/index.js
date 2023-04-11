be8ac503a162877aecea6a40253cca3b3e6e906f
Sigit Haryoyudanto <83746683+vvarl0cks@users.noreply.github.com>
2023-04-11 17:54:45 +0800

    backend

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x11dDE0431A6ecD4d571a6bcb14318E8427ECb0f4";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
